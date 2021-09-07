dd5bb1b5131999522c3bc28342fb395cec6fbd5c
Rost-Perig <rost.perig@gmail.com>
2021-09-07 08:50:59 +0300

    modal-service converted to class

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import LoadMoreBtn from './js/load-more-btn';
 import { pN  } from './js/pnotify-set';
 import ImagesApiService from './js/api-img-service';
 import { onSearch, fetchAndRenderImages } from './js/search-render';
-import { openModalClick, closeModalOn, onModalImageTurn, onCloseModalOverlay, onKeyPress } from './js/modal-service';
 import { toTop, scrollWatch, orientationValue, imgTypeValue, infiniteScrollValue, resetAll } from './js/utils';
 import { refs } from './js/refs';
 
@@ -15,17 +14,14 @@ export const loadMoreBtn = new LoadMoreBtn({
   hidden: true,
 });
 
+import ModalWindow from './js/modal-service'; 
+const modalWind = new ModalWindow; 
+modalWind.createMarkupAndEvnListeners(refs);
+
 window.addEventListener('scroll', throttle(500, e => scrollWatch(refs)));
-window.addEventListener('keydown', e => onKeyPress(e, refs));
-refs.modalImage.addEventListener('mousemove', throttle(500, e=> refs.xMousePosition = e.clientX));
-refs.modalImage.addEventListener('click', () => onModalImageTurn(refs));
-refs.btnReset.addEventListener('click', () => resetAll(refs));
 refs.doneBtn.addEventListener('click', () => resetAll(refs));
 refs.searchForm.addEventListener('submit', e => onSearch(e, refs, pN));
 loadMoreBtn.refs.button.addEventListener('click', () => fetchAndRenderImages(refs, pN));
-refs.imgList.addEventListener('click', e => openModalClick(e, refs));
-refs.modalCloser.addEventListener('click', () => closeModalOn(refs));
-refs.modalOverlay.addEventListener('click', e => onCloseModalOverlay(e, refs));
 refs.toTopBtn.addEventListener('click', () => toTop(refs));
 refs.formOrientation.addEventListener('input',() => orientationValue(refs));
 refs.formImgType.addEventListener('input', () => imgTypeValue(refs));
--- src/js/modal-service.js
+++ src/js/modal-service.js
@@ -1,110 +1,141 @@
-/* ======================== работа с модальным окном ======================== */
+import { throttle } from '../../node_modules/throttle-debounce';
 
-// остановка скролла под модальным окном
-export function stopScroll() {
-  document.body.style.overflow = "hidden";
-  document.body.style.height = "100wh";
-};
+export default class ModalWindow {
+    constructor() {
+        this.markupString = '<div class="modal js-modal"><div div class="modal__overlay"></div ><div class="modal__content"><div class="img-card"><img src="" alt="" class="img-card__image"/></div></div><button type="button" class="modal__button" data-action="close-modal"></button></div > ';
+        this.modal;
+        this.modalContent;
+        this.modalCloser;
+        this.modalOverlay;
+        this.modalImage;
+        this.tempModalImgUrls = null;
+        this.xMousePosition = 0;
+    };
+    
+    /* ============создание разметки и слушателя событий============ */
 
-// запуск скролла после закрытия модального окна
-export function startScroll() {
-  document.body.style.overflow = "auto"; 
-  document.body.style.height = "auto";
-}
+    createMarkupAndEvnListeners(ref) {
+        document.querySelector('script').insertAdjacentHTML("beforebegin", this.markupString);
+        this.modal = document.querySelector('.js-modal');
+        this.modalContent = document.querySelector('.modal__content');
+        this.modalCloser = document.querySelector('button[data-action="close-modal"]');
+        this.modalOverlay = document.querySelector('.modal__overlay');
+        this.modalImage = document.querySelector('.img-card__image');
+        this.createAddEvtListenerModal(ref);
+    };
 
-export function openModalOn({modal, modalImage}) {
-  modal.classList.add('is-open');
-  modalImage.src = document.activeElement.parentNode.dataset.url;
-  stopScroll();
-};
+    createAddEvtListenerModal({imgList}) {
+        this.modalImage.addEventListener('click', () => this.onModalImageTurn());
+        this.modalCloser.addEventListener('click', () => this.closeModalOn());
+        this.modalOverlay.addEventListener('click', e => this.onCloseModalOverlay(e));
+        this.modalImage.addEventListener('mousemove', throttle(500, e => this.xMousePosition = e.clientX));
+        window.addEventListener('keydown', e => this.onKeyPress(e));
+        imgList.addEventListener('click', () => this.openModalOn());
+    };
 
-export function openModalClick(e, { modal, modalImage }) {
-  if (e.target.classList.contains('main-img')) {
-   modal.classList.add('is-open');
-  modalImage.src = e.target.parentNode.parentNode.dataset.url;
-  stopScroll(); 
-  } else {
-    return
-  }
-  
-};
- 
-export function closeModalOn({modal, modalImage}) {
-  modal.classList.remove('is-open');
-  modalImage.src = "";
-  startScroll();
-};
+    /* ============остановка скролла под модальным окном============ */
+    stopScroll() {
+    document.body.style.overflow = "hidden";
+    document.body.style.height = "100wh";
+    };
 
-export function onCloseModalOverlay(e, ref) {
-  if (e.target === e.currentTarget) {
-    closeModalOn(ref);
-  }
-};
+    /* =========запуск скролла после закрытия модального окна========= */
 
-/* ======================== перелистывание картинок в модальном окне ======================== */
+    startScroll() {
+    document.body.style.overflow = "auto"; 
+    document.body.style.height = "auto";
+    };
 
-export function modalImageRight({tempImgURLs, modalImage}) {
-  const curImgIndex = tempImgURLs.indexOf(tempImgURLs.find(tempImgURL => tempImgURL === modalImage.src));
+    /* =========создание массива ссылок на большие картинки========= */
 
-  let nextImgIndex = 0;
+    createTempImgUrls() {
+    this.tempModalImgUrls = [...document.querySelectorAll('.img-list-link')].map(el => el.parentNode.dataset.url);
+    };  
+        
+    openModalOn() {
+    this.modal.classList.add('is-open');
+    this.modalImage.src = document.activeElement.parentNode.dataset.url;
+        this.stopScroll();
+    this.createTempImgUrls();    
+    };
 
-  curImgIndex === tempImgURLs.length - 1 ?
-    nextImgIndex = 0 :
-    nextImgIndex = curImgIndex + 1;
+    /* =========открытие-закрытие модального окна========= */
 
-  modalImage.src = `${tempImgURLs[nextImgIndex]}`;
-};
+    closeModalOn() {
+    this.modal.classList.remove('is-open');
+    this.modalImage.src = "";
+    this.startScroll();
+    this.tempModalImgUrls = null;
+    };
 
-export function modalImageLeft({tempImgURLs, modalImage}) {
-const curImgIndex = tempImgURLs.indexOf(tempImgURLs.find(tempImgURL => tempImgURL === modalImage.src));
-
-  let previousImgIndex = 0;
-
-  curImgIndex === 0 ?
-    previousImgIndex = tempImgURLs.length - 1 :
-    previousImgIndex = curImgIndex - 1;
-
-  modalImage.src = `${tempImgURLs[previousImgIndex]}`;
-};
-
-/* ======================== перелистывание картинок в модальном окне клиом мыши ======================== */
-
-export function onModalImageTurn(ref) {
-  if (ref.xMousePosition > (window.innerWidth / 2)) {
-    modalImageRight(ref);
-  } else {
-    modalImageLeft(ref);
-  };
-};
-
-/* ======================== события кнопок ======================== */
-
-export function onKeyPress(e, ref) {
-  if (e.code === "Enter" && document.activeElement.classList.contains("img-list-link")) {
-    // e.preventDefault();
-    openModalOn(ref);
-  };
-
-  let modalIsOpen = ref.modal.classList.contains('is-open');
-  
-  if (modalIsOpen) {
-    switch (e.code) {
-      case "Escape": 
-       closeModalOn(ref);
-       break;
-
-      case "ArrowRight":
-       modalImageRight(ref);
-       break;
-
-      case "ArrowLeft":
-       modalImageLeft(ref);
-       break;
-
-      default:
-       break;
+    onCloseModalOverlay(e) {
+    if (e.target === e.currentTarget) {
+        this.closeModalOn();
     }
-  };
+        };
+        
+    /* =========== перелистывание картинок в модальном окне =========== */
+
+    modalImageRight() {
+    const curImgIndex = this.tempModalImgUrls.indexOf(this.tempModalImgUrls.find(tempImgURL => tempImgURL === this.modalImage.src));
+    let nextImgIndex = 0;
+
+    curImgIndex === this.tempModalImgUrls.length - 1 ?
+        nextImgIndex = 0 :
+        nextImgIndex = curImgIndex + 1;
+
+    this.modalImage.src = `${this.tempModalImgUrls[nextImgIndex]}`;
+    };
+
+    modalImageLeft() {
+    const curImgIndex = this.tempModalImgUrls.indexOf(this.tempModalImgUrls.find(tempImgURL => tempImgURL === this.modalImage.src));
+    let previousImgIndex = 0;
+
+    curImgIndex === 0 ?
+        previousImgIndex = this.tempModalImgUrls.length - 1 :
+        previousImgIndex = curImgIndex - 1;
+
+    this.modalImage.src = `${this.tempModalImgUrls[previousImgIndex]}`;
+    };
+
+    /* ========== перелистывание картинок в модальном окне клиом мыши ========== */
+
+    onModalImageTurn() {
+    if (this.xMousePosition > (window.innerWidth / 2)) {
+        this.modalImageRight();
+    } else {
+        this.modalImageLeft();
+    };
+    };
+
+    /* ================ события кнопок ================ */
+
+    onKeyPress(e) {
+    if (e.code === "Enter" && document.activeElement.classList.contains("img-list-link")) {
+        e.preventDefault();
+        this.openModalOn();
+    };
+
+    let modalIsOpen = this.modal.classList.contains('is-open');
+    
+    if (modalIsOpen) {
+        switch (e.code) {
+        case "Escape": 
+        this.closeModalOn();
+        break;
+
+        case "ArrowRight":
+        this.modalImageRight();
+        break;
+
+        case "ArrowLeft":
+        this.modalImageLeft();
+        break;
+
+        default:
+        break;
+        }
+    };
+    };
 };
-
-
+    
